860d416fb8c9e5c71fbcee06e4d6907e62ba552f
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2024-08-11 00:12:22 -0700

    s

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var acc = document.getElementsByClassName("droptitle");
+var paths = document.querySelectorAll("path")
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      paths[i].setAttribute("d","M19.5 8.25l-7.5 7.5-7.5-7.5")
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      paths[i].setAttribute("d","M4.5 15.75l7.5-7.5 7.5 7.5")
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
   // array with texts to type in typewriter
   var dataText = ['East Bay Hacks', 'Also known as HastinHacks'];
